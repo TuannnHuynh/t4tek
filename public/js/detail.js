38791f57e62409388a53df8a6e82bbe6a0a1d389
Tuấn Huỳnh Anh <huynhanhtuan8547@gmail.com>
2025-02-10 16:00:15 +0700

    Final page detail course

---
+++ public/js/detail.js
@@ -0,0 +1,19 @@
+$(".item").click(function () {
+  $(".item").removeClass("active");
+  $(this).addClass("active");
+});
+
+$(".title-content").click(function () {
+  var mainContent = $(this).next(".main-content");
+  var icon = $(this).find("i");
+
+  if (mainContent.is(":visible")) {
+    mainContent.slideUp();
+    icon.removeClass("fa-minus").addClass("fa-plus");
+  } else {
+    $(".main-content").slideUp();
+    $(".title-content i").removeClass("fa-minus").addClass("fa-plus"); // Đặt lại icon
+    mainContent.slideDown();
+    icon.removeClass("fa-plus").addClass("fa-minus");
+  }
+});
