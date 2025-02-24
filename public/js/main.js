6e14ee23b85a80e02deb65b387e37b898469654d
Tuấn Huỳnh Anh <huynhanhtuan8547@gmail.com>
2025-02-24 22:21:24 +0700

    Add page Detail Short

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+// page chi tiet
+$(".title-content").click(function () {
+  var mainContent = $(this).next(".main-content");
+  var icon = $(this).find("i");
 
+  if (mainContent.is(":visible")) {
+    mainContent.slideUp();
+    icon.removeClass("fa-minus").addClass("fa-plus");
+  } else {
+    $(".main-content").slideUp();
+    $(".title-content i").removeClass("fa-minus").addClass("fa-plus"); // Đặt lại icon
+    mainContent.slideDown();
+    icon.removeClass("fa-plus").addClass("fa-minus");
+  }
+});
