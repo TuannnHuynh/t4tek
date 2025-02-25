2d9fe87ec50dbae775603f782355bda280b4d711
Tuấn Huỳnh Anh <huynhanhtuan8547@gmail.com>
2025-02-25 16:16:07 +0700

    Edit page Detail Short

--- public/js/detail-short.js
+++ public/js/detail-short.js
@@ -2,3 +2,21 @@ $(".item").click(function () {
   $(".item").removeClass("active");
   $(this).addClass("active");
 });
+
+$(".subnav ul li").click(function () {
+  $("li").removeClass("active");
+  $(this).addClass("active");
+});
+
+$("li .title").click(function () {
+  var lesson = $(this).next(".lesson");
+  var icon = $(this).find("i");
+
+  if (lesson.is(":visible")) {
+    lesson.slideUp();
+    icon.removeClass("fa-minus").addClass("fa-plus");
+  } else {
+    lesson.slideDown();
+    icon.removeClass("fa-plus").addClass("fa-minus");
+  }
+});
